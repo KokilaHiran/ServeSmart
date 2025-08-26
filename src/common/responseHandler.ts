e1f16cfedf68a8aa175238d78c019f0ac55138ef
iamkokilahiran <priyadarshanarakh@gmail.com>
2025-08-26 15:07:50 +0530

    add .env file and implement response handler with success and error response functions

---
+++ src/common/responseHandler.ts
@@ -0,0 +1,17 @@
+import { Response } from "express";
+import { HttpStatus } from "./constants/httpStatus.enum";
+import { IError } from "../interface/error.interface";
+
+export const successResponse = (code:HttpStatus,response:Response,data:any , alreadyWrappedWithData:boolean) => {
+    let responseData = alreadyWrappedWithData ? data : { data };
+    response.status(code).json(responseData);
+}
+
+export const errorResponse = (code:HttpStatus,response:Response, error:IError, exactError?:IError) => {
+    const  errorcontent = {
+        code,
+        key:error.key,
+        message:error.message,
+    }
+    response.status(code).json({ error: errorcontent });
+}
